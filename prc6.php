5c552bc7a0c657e61f4a38d7b56e6eeae1b3414f
Humayun Khan <humayunk.pvt@gmail.com>
2023-03-23 23:25:22 +0530

    Practical 06 added

--- prc6.php
+++ prc6.php
@@ -44,13 +44,39 @@
             $this->property2 = $arg2;
         }
     }
-    $obj = new MyClass('Humayun ', 'Khan');
+    $obj = new MyClass('Humayun ', 'Khan<br>');
     echo $obj->property1;
     echo $obj->property2;
 
     // Protected Access Modifier code
     /* Parent Class (Also called superclass and base class) */
-    
+    class Phone
+    {
+        public $name;
+        public $color;
+
+        function __construct($name, $color)
+        {
+            $this->name = $name;
+            $this->color = $color;
+        }
+
+        protected function intro()
+        {
+            echo "The phone is {$this->name} and the color is {$this->color}.";
+        }
+    }
+    /* Child class (Also called derived class,  extended class, and subclass) */
+    class Apple extends Phone
+    {
+        function message()
+        {
+            echo "About Phone: ";
+            $this->intro();
+        }
+    }
+    $obj = new Apple("Apple iPhone 14 Pro", "Green");
+    $obj->message();
     ?>
 </body>
 
