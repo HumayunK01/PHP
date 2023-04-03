3fd44d476766f02bf4db20217fbf2d714c342edc
Humayun Khan <humayunk.pvt@gmail.com>
2023-04-03 23:58:33 +0530

    init

--- prc08.php
+++ prc08.php
@@ -5,23 +5,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+        rel="stylesheet" />
     <title>Practical 08</title>
 </head>
 
-<body>
+<body style="font-family: 'Poppins';">
+    <!-- ------ Code for List Box ------ -->
+    <form action="" method="post">
+        <select name="foods[]" size="5">
+            <option value="Tomatoes">Tomatoes</option>
+            <option value="Cucumbers">Cucumbers</option>
+            <option value="Celery">Celery</option>
+            <option value="Radish">Radish</option>
+            <option value="Onion">Onion</option>
+        </select><br>
+        <input type="submit" name="submit" value="Submit" />
+    </form>
     <?php
-    $toy_cars = array("Pull Back Cars", "Remote Cars", "Electric Cars", "Toy Race Cars", "Lightening Cars");
-    $array_length = count($toy_cars);
-    ?>
-    <select name="dynamic_data">
-        <?php
-        for ($i = 0; $i < $array_length; $i++) {
-            ?>
-            <option value="<?= $toy_cars[$i]; ?>"><?= $toy_cars[$i]; ?></option>
-            <?php
+    $c = $_POST['foods'];
+    if (isset($c)) {
+        echo 'You have chosen ';
+        foreach ($c as $key => $value) {
+            echo $value;
         }
-        ?>
-    </select>
+    } else {
+        echo "You haven't selected any foods";
+    }
+    ?>
+
+    <!-- ------ Code for Combo Box ------ -->
+    <form action="" method="post">
+        <select name="foods2[]">
+            <option value="Mango">Mango</option>
+            <option value="Banana">Banana</option>
+            <option value="Watermelon">Watermelon</option>
+            <option value="Grapes">Grapes</option>
+        </select><br>
+        <input type="submit" name="submit" value="Submit" />
+    </form>
+    <?php
+    $c = $_POST['foods2'];
+    if (isset($c)) {
+        echo 'You have chosen ';
+        foreach ($c as $key => $value) {
+            echo $value;
+        }
+    } else {
+        echo "You haven't selected any fruit";
+    }
+    ?>
 </body>
 
 </html>
